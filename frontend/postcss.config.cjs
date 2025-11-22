70e480c043961f059ee73dc168a05cce37b682cf
Ansh0226 <ansh.114.ar@gmail.com>
2025-11-22 19:27:57 +0530

    Delete : remove extra files and code and make code base clean

--- frontend/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {}, // ✅ correct Tailwind v4 PostCSS plugin
-    autoprefixer: {}, // optional but fine to keep
-  },
-};
